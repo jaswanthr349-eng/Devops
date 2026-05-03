689c285d7ea92a069e26ab63e1d20199aa2cc807
gitadmin <abc@gmail.com>
2026-05-03 17:14:37 +0530

    https.py file has been editied with code

---
+++ https.py
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+ <title>My Sample Project</title>
+ <link rel="stylesheet" href="style.css">
+ </head>
+ <body> 
+<h1>Hello DevOps 🚀</h1>
+ <button 
+onclick="showMessage()">Click Me</button>
+ <script src="script.js"></script>
+ </body>
+</html>
